16ccebdea58c5ced025b027aee39fb9b4cc773b6
Ramon Prieto <ramonprieto12@gmail.com>
2024-01-09 10:22:39 -0600

    Update __init__.py

--- tap_gitlab/__init__.py
+++ tap_gitlab/__init__.py
@@ -249,29 +249,40 @@ def get_start(entity):
     return STATE[entity]
 
 
-@backoff.on_exception(backoff.expo,
-                      (requests.exceptions.RequestException),
-                      max_tries=5,
-                      giveup=lambda e: e.response is not None and 400 <= e.response.status_code < 500, # pylint: disable=line-too-long
-                      factor=2)
+@backoff.on_exception(
+    backoff.expo,
+    (requests.exceptions.RequestException),
+    max_tries=5,
+    giveup=lambda e: e.response is not None
+    and (
+        400 <= e.response.status_code < 500 and e.response.status_code != 429
+    ),  # pylint: disable=line-too-long
+    factor=2,
+)
 def request(url, params=None):
     params = params or {}
 
-    headers = { "Private-Token": CONFIG['private_token'] }
-    if 'user_agent' in CONFIG:
-        headers['User-Agent'] = CONFIG['user_agent']
+    headers = {"Private-Token": CONFIG["private_token"]}
+    if "user_agent" in CONFIG:
+        headers["User-Agent"] = CONFIG["user_agent"]
 
-    resp = SESSION.request('GET', url, params=params, headers=headers)
+    resp = SESSION.request("GET", url, params=params, headers=headers)
     LOGGER.info("GET {}".format(url))
 
     if resp.status_code in [401, 403, 404]:
         LOGGER.info("Skipping request to {}".format(url))
         LOGGER.info("Reason: {} - {}".format(resp.status_code, resp.content))
         raise ResourceInaccessible
+    elif resp.status_code == 429:
+        LOGGER.warning("429 - Rate limit exceeded. Waiting for 60 seconds...")
+        time.sleep(60)
+        raise requests.exceptions.RequestException
     elif resp.status_code >= 400:
         LOGGER.critical(
             "Error making request to GitLab API: GET {} [{} - {}]".format(
-                url, resp.status_code, resp.content))
+                url, resp.status_code, resp.content
+            )
+        )
         sys.exit(1)
 
     return resp
